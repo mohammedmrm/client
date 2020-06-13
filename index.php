d0d764fa1de8cf4b22e1703a552e87767aaea2f1
MohammedIT94 <mohammed.rbn4@yahoo.com>
2020-06-13 18:52:35 +0300

    some update

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-if($_SESSION['login'] != 1){
-    header('location: login.php');
-}
 require_once("php/_access.php");
+access();
 require_once("config.php");
 ?>
 <!DOCTYPE HTML>
--- recived.php
+++ recived.php
@@ -1,7 +1,3 @@
-<?php
-
-require_once("config.php");
-?>
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
