9cb418de7bdb4966bb239d14e4f40fdbe43645d8
MohammedIT94 <mohammed.rbn4@yahoo.com>
2020-06-14 00:29:43 +0300

    bugs fix

--- login.php
+++ login.php
@@ -6,10 +6,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=2, viewport-fit=cover" />
-    <meta name="description" content="في هذه الصفحة الرئيسية لشركة النهر تستطيع ان تتعرف على الطلبيات الخاصة بك الواصة والراجعة والكثير من المعلومات">
-    <meta name="الصفحة الرئيسية للعميل" property="og:title" content="معلومات متكاملة للعميل في هذه الصفحة خاصة بعملاء شركة النهر">
+    <meta name="description" content="في هذه الصفحة الرئيسية <?php echo($config['Company_name'];)?> تستطيع ان تتعرف على الطلبيات الخاصة بك الواصة والراجعة والكثير من المعلومات">
+    <meta name="<?php echo($config['Company_name'];)?>" property="og:title" content="معلومات متكاملة للعميل في هذه الصفحة خاصة بعملاء  <?php echo($config['Company_name'];)?>">
 
-    <title>شركة النهر للحلول البرمجية</title>
+    <title><?php echo($config['Company_name'];)?></title>
     <link href="https://fonts.googleapis.com/css?family=Cairo:300,300i,400,400i,500,500i,700,700i,900,900i|Source+Sans+Pro:300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="styles/style.css">
     <link rel="stylesheet" type="text/css" href="styles/framework.css">
