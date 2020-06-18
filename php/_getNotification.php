3e5b94535d172dae07f135cf06563fe3f0fb8cff
MohammedIT94 <mohammed.rbn4@yahoo.com>
2020-06-18 22:34:44 +0300

    wellcome message

--- php/_getNotification.php
+++ php/_getNotification.php
@@ -15,7 +15,7 @@ $user_id = $_SESSION['userid'];
 $sql = 'select count(*) as unseen from notification where for_client = 1 and client_id = ? and client_seen=0';
 $res = getData($con,$sql,[$user_id]);
 $unseen = $res[0]['unseen'];
-$sql = 'select * from notification where for_client = 1 and client_id = ? order by date DESC limit 50';
+$sql = 'select notification.*,orders.order_no from notification inner join orders on orders.id = notification.order_id where for_client = 1 and notification.client_id = ? order by date DESC limit 50';
 $result = getData($con,$sql,[$user_id]);
 $success = 1;
 echo json_encode([$user_id,'success'=>$success,"data"=>$result,'unseen'=>$unseen]);
