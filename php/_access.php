46a5ba6a12610ec28551800287b6488623777dc4
MohammedIT94 <mohammed.rbn4@yahoo.com>
2020-06-28 22:33:11 +0300

    cookie added

--- php/_access.php
+++ php/_access.php
@@ -2,10 +2,10 @@
 if(!isset($_SESSION)){
 session_start();
 }
-require_once("dbconnection.php");                          
+require_once("dbconnection.php");
 function access(){
   if(!empty($_COOKIE['username']) && !empty($_COOKIE['password'])){
-    $sql = "select staff.*,role.home as home from staff inner join role on role.id = staff.role_id where phone = ? and password =? and status=1";
+    $sql = "select clients.* from staff  where phone = ? and password =? and status=1";
     $result = getData($GLOBALS['con'],$sql,[$_COOKIE['username'],$_COOKIE['password']]);
   }
   if(count($result)> 0){
