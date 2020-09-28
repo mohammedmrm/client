0f8fdb9d72ffc45b6bf2da54ddcb7a82b7f9ed3e
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-09-28 22:10:50 +0300

    api update

--- api/_apiAccess.php
+++ api/_apiAccess.php
@@ -2,7 +2,7 @@
 if(!isset($_SESSION)){
  session_start();
 }
-//die(json_encode(['message'=>"Access Deny"])); 
+//die(json_encode(['message'=>"Access Deny"]));
 error_reporting(0);
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
