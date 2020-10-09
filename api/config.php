4faf3c137077de42013a23b6bbc0d8a263b48999
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-10-09 21:07:54 +0300

    api update

--- api/config.php
+++ api/config.php
@@ -1,18 +1,32 @@
 <?php
-$config = [
-   "Company_name"=>"شركة النهر",
-   "Company_phone"=>"0782222222",
-   "wellcome_message"=>"اعلان",
-   "Company_email"=>"nahar@nahar.com",
-   "Company_logo"=>"img/logos/logo.png",
-   "dev_b"=>5000,               //??? ??????? ?????
-   "dev_o"=>10000,               //??? ??????? ????? ?????????
-   "weightPrice"=>1000         //??? ??????? ????? ?????????
-];
+//session_start();
+error_reporting(0);
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
 require_once("../php/dbconnection.php");
-$sql = "select * from setting";
-$setting = getData($con,$sql);
-foreach($setting as $val){
-  $config[$val['control']] =  $val['value'];
+require_once("_apiAccess.php");
+access();
+try{
+  $msg="";
+  $config = [
+     "Company_name"=>"شركة النهر",
+     "Company_phone"=>"0782222222",
+     "wellcome_message"=>"اعلان",
+     "Company_email"=>"nahar@nahar.com",
+     "Company_logo"=>"img/logos/logo.png",
+     "dev_b"=>5000,                 //??? ??????? ?????
+     "dev_o"=>10000,                //??? ??????? ????? ?????????
+     "weightPrice"=>1000,           //??? ??????? ????? ?????????
+  ];
+  $sql = "select * from setting";
+  $setting = getData($con,$sql);
+  foreach($setting as $val){
+    $config[$val['control']] =  $val['value'];
+  }
+}catch(PDOException $ex) {
+   $data=["error"=>$ex];
+   $success="0";
+   $msg = "Query Error";
 }
+echo json_encode(['code'=>200,'message'=>$msg,"success"=>$success,'config'=>$config]);
 ?>
