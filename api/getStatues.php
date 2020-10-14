fe17580931384a85cea9327a87b074c9b1d6dfcd
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-10-14 20:55:50 +0300

    api update

---
+++ api/getStatues.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+error_reporting(0);
+header("Access-Control-Allow-Origin: *");  
+header('Content-Type: application/json');
+require_once("_apiAccess.php");
+access();
+$msg="";
+require_once("../php/dbconnection.php");
+try{
+  $query = "select * from order_status";
+  $data = getData($con,$query);
+  $success="1";
+} catch(PDOException $ex) {
+   $data=["error"=>$ex];
+   $success="0";
+   $msg = "Query Error";
+}
+echo (json_encode(array('code'=>200,'message'=>$msg,"success"=>$success,"data"=>$data),JSON_PRETTY_PRINT));
+?>
