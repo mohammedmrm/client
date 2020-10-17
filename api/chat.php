341fac01b631477c0246a41972c828ee97e22ccb
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-10-17 22:04:41 +0300

    bugs

--- api/chat.php
+++ api/chat.php
@@ -19,6 +19,16 @@ if(empty($page)){
 }
 
 try {
+    $count = "select count(*) as count from orders
+            inner join (
+             select max(id) as msg_id ,max(order_id) as order_id from message
+             GROUP by message.order_id
+
+            ) a on a.order_id = orders.id
+
+            inner join message on a.msg_id = message.id
+            where orders.client_id = ?   and orders.confirm = 1 and invoice_id = 0";
+
     $sql = "select message.client_seen,orders.order_no,orders.id,message.id as msg_id,message.message as message,message.date from orders
             inner join (
              select max(id) as msg_id ,max(order_id) as order_id from message
@@ -27,10 +37,12 @@ try {
             ) a on a.order_id = orders.id
 
             inner join message on a.msg_id = message.id
-            where orders.client_id = ?
+            where orders.client_id = ? and orders.confirm = 1  and invoice_id = 0
             order by message.date";
     $lim = " limit ".(($page-1) * $limit).",".$limit;
     $sql .= $lim;
+    $count = getData($con,$count,[$userid]);
+    $pages= ceil($count[0]['count'] / $limit);
     $data = getData($con,$sql,[$userid]);
     $success="1";
 }catch(PDOException $ex) {
@@ -39,5 +51,5 @@ try {
     $msg ="Query Error";
 }
 ob_end_clean();
-echo json_encode(['code'=>200,'message'=>$msg,'success'=>$success,"data"=>$data]);
+echo json_encode(['code'=>200,'message'=>$msg,'success'=>$success,"data"=>$data,"page"=>$page,'pages'=>$pages,'count'=>$count[0]["count"]]);
 ?>
