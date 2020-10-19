2f9e82439a245d235b284b33093dbfc65b7de72b
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-10-19 22:29:36 +0300

    api

--- api/_sendNoti.php
+++ api/_sendNoti.php
@@ -37,16 +37,6 @@
             'Content-Type: application/json'
         ];
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$fcmUrl);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
-        $result = curl_exec($ch);
-        curl_close($ch);
-
         $notification = ['body' => $body];
         require_once '../vendor/autoload.php';
         $channelName = 'chat-messages';
@@ -59,6 +49,16 @@
         }
         // Notify an interest with a notification
         $r = $expo->notify([$channelName], $notification);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$fcmUrl);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
+        $result = curl_exec($ch);
+        curl_close($ch);
         $f = [$result,$r];
 
         return $f;
