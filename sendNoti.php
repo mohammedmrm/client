f18756a9ac390c7cb822feeb04fa9ddfc09a6e7f
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-10-20 17:10:50 +0300

    bugs

--- sendNoti.php
+++ sendNoti.php
@@ -1,5 +1,5 @@
 <?php
- require_once 'vendor/autoload.php';
+ require_once __DIR__.'/vendor/autoload.php';
  try{
   $channelName = 'chat-messages';
   $recipient= 'ExponentPushToken[bLxtxLPkcaF3IbjFeYAkUn]';
