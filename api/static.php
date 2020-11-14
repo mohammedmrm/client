efb8063fc8f6b23e6f7624a65c3c2cef871f5727
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-11-14 20:46:53 +0300

    api

--- api/static.php
+++ api/static.php
@@ -86,9 +86,9 @@ try{
     }
     $sql = "SELECT
           SUM(IF (order_status_id = '1' or order_status_id = '2' or order_status_id = '3' or order_status_id = '13',1,0)) as  onway,
-          SUM(IF (order_status_id = '9',1,0)) as  inprocess,
-          SUM(IF (order_status_id = '6',1,0)) as  partiallyReturnd,
-          SUM(IF (order_status_id = '5',1,0)) as  `replace`,
+          SUM(IF (order_status_id = '9' and storage_id<>1,1,0)) as  inprocess,
+          SUM(IF (order_status_id = '6' and storage_id<>1,1,0)) as  partiallyReturnd,
+          SUM(IF (order_status_id = '5' and storage_id<>1,1,0)) as  `replace`,
           SUM(IF ((order_status_id = '9') and storage_id=1,1,0)) as  instorageReturnd,
           SUM(IF ((order_status_id = '6') and storage_id=1,1,0)) as  instoragepartiallyReturnd,
           SUM(IF ((order_status_id = '5') and storage_id=1,1,0)) as  instoragereplace,
