344f9a782349a4cb96e7d39a51cbfdb51412e566
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2022-08-27 23:46:10 +0200

    bug fix

--- api/static.php
+++ api/static.php
@@ -11,6 +11,7 @@ require_once("../config.php");
 $msg = "";
 $start30 = date('Y-m-d 00:00:00', strtotime(' - 30 day'));
 $end30 = date('Y-m-d 00:00:00', strtotime(' + 1 day'));
+
 try {
     $sql30 = "select
                 SUM(IF (order_status_id = '1' or order_status_id = '2' or order_status_id = '3' or order_status_id = '13',1,0)) as  onway,
