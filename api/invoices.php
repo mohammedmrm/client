208bade0f3cb7f90fb3128b383e4ad0596711c90
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2022-11-13 21:23:27 +0100

    re calculate delivery price

--- api/invoices.php
+++ api/invoices.php
@@ -66,9 +66,15 @@ try {
     $sql = "select
           sum(new_price) as income,
           sum(if(to_city = 1,
-                  if(orders.order_status_id=9,0,if(client_dev_price.price is null,(" . $config['dev_b'] . " - discount),(client_dev_price.price - discount))),
-                  if(orders.order_status_id=9,0,if(client_dev_price.price is null,(" . $config['dev_o'] . " - discount),(client_dev_price.price - discount)))
-                 )
+                      if(towns.center = 1,
+                        if(client_dev_price.price is null,(" . $config['dev_b'] . " - discount),(client_dev_price.price - discount)),
+                        if(client_dev_price.town_price is null,(" . ($config['dev_b'] + $config['countrysidePrice']) . " - discount),(client_dev_price.town_price - discount))
+                      ),
+                      if(towns.center = 1,
+                        if(client_dev_price.price is null,(" . $config['dev_o'] . " - discount),(client_dev_price.price - discount)),
+                        if(client_dev_price.town_price is null,(" . ($config['dev_o'] + $config['countrysidePrice']) . " - discount),(client_dev_price.town_price - discount))
+                      )
+                  )
                  + if(new_price > 500000 ,( (ceil(new_price/500000)-1) * " . $config['addOnOver500'] . " ),0)
                  + if(weight > 1 ,( (weight-1) * " . $config['weightPrice'] . " ),0)
                  + if(towns.center = 0 ," . $config['countrysidePrice'] . ",0)
@@ -76,8 +82,14 @@ try {
           sum(new_price -
               (
                 if(to_city = 1,
-                  if(orders.order_status_id=9,0,if(client_dev_price.price is null,(" . $config['dev_b'] . " - discount),(client_dev_price.price - discount))),
-                  if(orders.order_status_id=9,0,if(client_dev_price.price is null,(" . $config['dev_o'] . " - discount),(client_dev_price.price - discount)))
+                      if(towns.center = 1,
+                        if(client_dev_price.price is null,(" . $config['dev_b'] . " - discount),(client_dev_price.price - discount)),
+                        if(client_dev_price.town_price is null,(" . ($config['dev_b'] + $config['countrysidePrice']) . " - discount),(client_dev_price.town_price - discount))
+                      ),
+                      if(towns.center = 1,
+                        if(client_dev_price.price is null,(" . $config['dev_o'] . " - discount),(client_dev_price.price - discount)),
+                        if(client_dev_price.town_price is null,(" . ($config['dev_o'] + $config['countrysidePrice']) . " - discount),(client_dev_price.town_price - discount))
+                      )
                  )
                  + if(new_price > 500000 ,( (ceil(new_price/500000)-1) * " . $config['addOnOver500'] . " ),0)
                  + if(weight > 1 ,( (weight-1) * " . $config['weightPrice'] . " ),0)
