10b7e849d88aa086c0c5d4d5fadb2eb6a9cb383b
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2023-01-21 09:52:27 +0100

    support client assistant

--- api/_apiAccess.php
+++ api/_apiAccess.php
@@ -1,6 +1,6 @@
 <?php
-if(!isset($_SESSION)){
- session_start();
+if (!isset($_SESSION)) {
+   session_start();
 }
 //die(json_encode(['message'=>"Access Deny"]));
 error_reporting(0);
@@ -10,40 +10,40 @@ $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
 $token = $_REQUEST['token'];
 require_once("../php/dbconnection.php");
-if(!empty($token)){
-  $sql = 'select * from clients where api_token=?';
-  $loginres  = getData($con,$sql,[$token]);
-  if(count($loginres) == 1){
-     $msg = 1;
-     $code = 200;
-     $userid = $loginres[0]['id'];
-     $showearnings =  $loginres[0]['show_earnings'];
-  }else{
-     $msg ="incorrect username or password";
-     $code = 300;
-  }
- }else if(!empty($password) && !empty($username)){
-  $sql = 'select * from clients where phone=?';
-  $loginres  = getData($con,$sql,[$username]);
-  if(count($loginres) == 1 && password_verify($password,$loginres[0]['password'])){
-     $msg = 1;
-     $code = 200;
-     $userid = $loginres[0]['id'];
-     $head_company_id= $loginres[0]['company_id'];
-     $showearnings =  $loginres[0]['show_earnings'];
-     $userrole= $loginres[0]['role_id'];
-  }else{
-     $msg ="incorrect username or password";
-     $code = 300;
-  }
-}else{
-     $msg ="username and password required";
-     $code = 301;
+if (!empty($token)) {
+   $sql = 'select * from clients where api_token=?';
+   $loginres  = getData($con, $sql, [$token]);
+   if (count($loginres) == 1) {
+      $msg = 1;
+      $code = 200;
+      $userid = $loginres[0]['type'] == 1 ?  $loginres[0]['id'] : $loginres[0]['client_id'];
+      $showearnings = $loginres[0]['type'] == 1 ? $loginres[0]['show_earnings'] : 0;
+   } else {
+      $msg = "incorrect username or password";
+      $code = 300;
+   }
+} else if (!empty($password) && !empty($username)) {
+   $sql = 'select * from clients where phone=?';
+   $loginres  = getData($con, $sql, [$username]);
+   if (count($loginres) == 1 && password_verify($password, $loginres[0]['password'])) {
+      $msg = 1;
+      $code = 200;
+      $userid = $loginres[0]['type'] == 1 ?  $loginres[0]['id'] : $loginres[0]['client_id'];
+      $head_company_id = $loginres[0]['company_id'];
+      $showearnings = $loginres[0]['type'] == 1 ? $loginres[0]['show_earnings'] : 0;
+      $userrole = $loginres[0]['role_id'];
+   } else {
+      $msg = "incorrect username or password";
+      $code = 300;
+   }
+} else {
+   $msg = "username and password required";
+   $code = 301;
 }
 
-function access(){
-  if($GLOBALS['msg']!=1){
-     die(json_encode(['message'=>$GLOBALS['msg'],'code'=>$GLOBALS['code']]));
-  }
+function access()
+{
+   if ($GLOBALS['msg'] != 1) {
+      die(json_encode(['message' => $GLOBALS['msg'], 'code' => $GLOBALS['code']]));
+   }
 }
-?>
