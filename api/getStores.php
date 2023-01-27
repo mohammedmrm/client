7d38d610be1889cdf2d9c196f47282e9496ec78b
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2023-01-27 10:10:07 +0100

    bug

--- api/getStores.php
+++ api/getStores.php
@@ -1,7 +1,6 @@
 <?php
-ob_start(); 
+ob_start();
 session_start();
-header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 require_once("_apiAccess.php");
 access();
@@ -9,17 +8,17 @@ error_reporting(0);
 require_once("../php/dbconnection.php");
 $id = $userid;
 $msg = "";
-try{
+try {
    $query = "select stores.*, clients.name as client_name , clients.phone as client_phone
    from stores inner join clients on clients.id = stores.client_id where stores.client_id=?";
-   $data = getData($con,$query,[$id]);
-   $success="1";
+   $data = getData($con, $query, [$id]);
+   $success = "1";
    $code = 200;
-} catch(PDOException $ex) {
-   $data=["error"=>$ex];
-   $success="0";
+} catch (PDOException $ex) {
+   $data = ["error" => $ex];
+   $success = "0";
    $msg = "Query Error";
    $code = 200;
 }
 ob_end_clean();
-echo (json_encode(['code'=>$code,'message'=>$msg,"success"=>$success,"data"=>$data],JSON_PRETTY_PRINT));
+echo (json_encode(['code' => $code, 'message' => $msg, "success" => $success, "data" => $data], JSON_PRETTY_PRINT));
