3ca95747a1f3e8b1d93d76d451d625606f409b80
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2023-01-27 09:52:06 +0100

    bug

--- api/_apiAccess.php
+++ api/_apiAccess.php
@@ -2,7 +2,6 @@
 if (!isset($_SESSION)) {
    session_start();
 }
-//die(json_encode(['message'=>"Access Deny"]));
 error_reporting(0);
 //header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
