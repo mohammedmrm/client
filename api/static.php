8c3153942409d689beaff988f267b5f2691865cd
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2023-01-27 09:50:23 +0100

    please

--- api/static.php
+++ api/static.php
@@ -2,7 +2,6 @@
 ob_start();
 session_start();
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
 header('Content-Type: application/json');
 require_once("_apiAccess.php");
 access();
