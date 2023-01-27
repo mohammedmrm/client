6b18d02031b35c33f79477e3f91e71acfbaf875c
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2023-01-27 10:13:08 +0100

    bug

--- api/search.php
+++ api/search.php
@@ -1,7 +1,6 @@
 <?php
 ob_start();
 session_start();
-header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 require_once("_apiAccess.php");
 access();
@@ -16,20 +15,20 @@ $status = trim($_REQUEST['status']);
 $limit = trim($_REQUEST['limit']);
 $page = trim($_REQUEST['page']);
 $orders = 0;
-if(empty($limit)){
- $limit = 10;
+if (empty($limit)) {
+  $limit = 10;
 }
-if(empty($page)){
- $page = 1;
+if (empty($page)) {
+  $page = 1;
 }
 
-if(empty($end)) {
-  $end = date('Y-m-d h:i:s', strtotime($end. ' + 1 day'));
-}else{
-   $end .=" 23:59:59";
+if (empty($end)) {
+  $end = date('Y-m-d h:i:s', strtotime($end . ' + 1 day'));
+} else {
+  $end .= " 23:59:59";
 }
-$start .=" 00:00:00";
-try{
+$start .= " 00:00:00";
+try {
   $count = "select count(*) as count from orders
             left join (
               select max(id) as last_id,order_id from tracking group by order_id
@@ -54,51 +53,51 @@ try{
             left join tracking on a.last_id = tracking.id
             ";
   $where = "where";
-  $filter = "orders.client_id =".$userid." and (orders.confirm=1) ";
-  if(!empty($search)){
-   $filter .= " and (order_no like '%".$search."%'
-                     or customer_name like  '%".$search."%'
-                     or customer_phone like '%".$search."%'
-                     or tracking.note like  '%".$search."%'
+  $filter = "orders.client_id =" . $userid . " and (orders.confirm=1) ";
+  if (!empty($search)) {
+    $filter .= " and (order_no like '%" . $search . "%'
+                     or customer_name like  '%" . $search . "%'
+                     or customer_phone like '%" . $search . "%'
+                     or tracking.note like  '%" . $search . "%'
                      )
                     ";
   }
   function validateDate($date, $format = 'Y-m-d H:i:s')
-    {
-        $d = DateTime::createFromFormat($format, $date);
-        return $d && $d->format($format) == $date;
-    }
-  if(validateDate($start) && validateDate($end)){
-      $filter .= " and date between '".$start."' AND '".$end."'";
-     }
-  if($city > 0){
-   $filter .= " and to_city =".$city;
+  {
+    $d = DateTime::createFromFormat($format, $date);
+    return $d && $d->format($format) == $date;
   }
-  if($store > 0){
-   $filter .= " and store_id =".$store;
+  if (validateDate($start) && validateDate($end)) {
+    $filter .= " and date between '" . $start . "' AND '" . $end . "'";
   }
-  if($status > 0){
-   $filter .= " and orders.order_status_id =".$status;
+  if ($city > 0) {
+    $filter .= " and to_city =" . $city;
   }
-  if($filter != ""){
+  if ($store > 0) {
+    $filter .= " and store_id =" . $store;
+  }
+  if ($status > 0) {
+    $filter .= " and orders.order_status_id =" . $status;
+  }
+  if ($filter != "") {
     $filter = preg_replace('/^ and/', '', $filter);
-    $filter = $where." ".$filter;
-    $count .= " ".$filter;
-    $query .= " ".$filter;
+    $filter = $where . " " . $filter;
+    $count .= " " . $filter;
+    $query .= " " . $filter;
   }
-  if($page != 0){
+  if ($page != 0) {
     $page = $page - 1;
   }
-  $query .= " order by orders.id DESC limit ".($page * $limit).",".$limit;
-  $data = getData($con,$query);
-  $ps = getData($con,$count);
+  $query .= " order by orders.id DESC limit " . ($page * $limit) . "," . $limit;
+  $data = getData($con, $query);
+  $ps = getData($con, $count);
   $orders = $ps[0]['count'];
-  $pages= ceil($ps[0]['count']/$limit);
-  $success="1";
-} catch(PDOException $ex) {
-   $data=["error"=>$ex];
-   $success="0";
-   $msg ="Query Error";
+  $pages = ceil($ps[0]['count'] / $limit);
+  $success = "1";
+} catch (PDOException $ex) {
+  $data = ["error" => $ex];
+  $success = "0";
+  $msg = "Query Error";
 }
 ob_end_clean();
-echo (json_encode(['code'=>200,'message'=>$msg,'orders'=>$orders,"success"=>$success,"data"=>$data,'pages'=>$pages,'nextPage'=>$page+2],JSON_PRETTY_PRINT));
+echo (json_encode(['code' => 200, 'message' => $msg, 'orders' => $orders, "success" => $success, "data" => $data, 'pages' => $pages, 'nextPage' => $page + 2], JSON_PRETTY_PRINT));
