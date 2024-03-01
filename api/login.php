aa0e2bf1e7faf5c22c87101efb7592e3094ee749
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2024-03-01 10:43:29 +0000

    fixing

--- api/login.php
+++ api/login.php
@@ -4,6 +4,7 @@ session_start();
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+header('Content-Type: application/json');
 error_reporting(0);
 session_start();
 $username = $_REQUEST['username'];
